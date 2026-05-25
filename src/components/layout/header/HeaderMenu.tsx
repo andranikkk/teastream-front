8e6804df67b782037025c7d3dca92560a789c5e2
Andranik Aivazyan <ayvazyanandranik46@gmail.com>
2026-05-25 23:09:43 +0400

    feat: create & adjust app header UI

---
+++ src/components/layout/header/HeaderMenu.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Button } from '@/components/ui/common/Button';
+import { useAuth } from '@/hooks/useAuth';
+import { useTranslations } from 'next-intl';
+import Link from 'next/link';
+
+export function HeaderMenu() {
+  const t = useTranslations('layout.headerMenu');
+
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <div className="ml-auto flex items-center gap-x-4">
+      {isAuthenticated ? (
+        <div>Profile menu</div>
+      ) : (
+        <>
+          <Link href="/account/login">
+            <Button variant="secondary">{t('login')}</Button>
+          </Link>
+          <Link href="/account/create">
+            <Button>{t('register')}</Button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
+}
